82102319c220e4b6c3e0e9e919b4d830183cad60
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-07-28 10:43:45 -0300

    feat: images.

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/template/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
+      <Hero />
     </main>
   )
 }
---
+++ my-app/src/template/hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+    return (
+      <section
+        style={{
+          backgroundImage: `url(${yourImageUrl})`,
+          backgroundSize: "cover",
+        }}
+      >
+        Hero
+      </section>
+    );
+}
